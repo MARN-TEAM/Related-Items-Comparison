ef14ddc3e786e7419122cea5da404eea4abd3e89
NadhemBacha <nadhembecha@gmail.com>
2021-03-24 10:52:16 +0100

    btn star is done

--- service/client/components/Card.jsx
+++ service/client/components/Card.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 
+{/* <button className="close-btn"><i className="times circle outline icon"></i></button> */}
 
 export default class Card extends Component {
     render() {
         return (            
             <div className="card card-customize"  style={{width: "18.5rem", height: "27rem"}}>
-                <button className="close-btn"><i className="times circle outline icon"></i></button>
+                <button className="star-btn"><i class="star outline icon"></i></button>
             <img className="card-img-top"  height='255px' src="https://www.invaluable.com/blog/wp-content/uploads/2018/05/Louis-Vuitton-Bags-Purses-Hero.jpg" alt="Card image cap" />
             
             <div className="card-body">
